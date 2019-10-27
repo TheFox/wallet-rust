fc94981ef7490fceebf875623d2d9fd7238f5309
Christian Mayer <christian@fox21.at>
2019-10-27 13:26:02 +0100

    Publish script dry-run.

---
+++ bin/publish.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+SCRIPT_BASEDIR=$(dirname "$0")
+
+which rustc &> /dev/null || { echo 'ERROR: rustc not found in PATH'; exit 1; }
+which cargo &> /dev/null || { echo 'ERROR: cargo not found in PATH'; exit 1; }
+
+cd "${SCRIPT_BASEDIR}/.."
+
+pushd wallet_lib/ &> /dev/null
+cargo publish --dry-run
+
+popd &> /dev/null
+cargo publish --dry-run
