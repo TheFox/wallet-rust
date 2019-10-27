368df70f701a883e08d71824826d1b40453b9087
Christian Mayer <christian@fox21.at>
2019-10-27 13:16:10 +0100

    Cargo infos.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,39 @@
 
 use std::env;
-// use wallet::
+use wallet_lib::command::CommandOptions;
+
+const APP_VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+// #[macro_use]
+// extern crate clap;
+// use clap::App;
+
+// // #[clap(version = "v1.0-beta")]
+// #[derive(Clap)]
+// struct Opts;
+
+extern crate clap;
+use clap::App;
 
 fn main() {
     println!("-> start");
 
     let args: Vec<String> = env::args().collect();
     println!("-> cmd: '{:?}'", args);
+
+    // let mut options = CommandOptions::new();
+    // println!("-> options: '{:?}'", options);
+
+    // Opts::parse();
+    // let opts: Opts = Opts::parse();
+
+    // println!("config: {}", opts.config);
+    // println!("Using input file: {}", opts.input);
+    // println!("file: {}", opts.file);
+
+    App::new("fake").version(APP_VERSION).get_matches();
+
+    // println!("APP_VERSION {}", APP_VERSION);
 
     println!("-> end");
 }
