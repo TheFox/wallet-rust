075ad9b90605fd04fee5bb620a6e6f429bbc8619
Christian Mayer <christian@fox21.at>
2019-10-27 12:03:12 +0100

    Helper scripts.

---
+++ bin/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+SCRIPT_BASEDIR=$(dirname "$0")
+
+which rustc &> /dev/null || { echo 'ERROR: rustc not found in PATH'; exit 1; }
+which cargo &> /dev/null || { echo 'ERROR: cargo not found in PATH'; exit 1; }
+
+cd "${SCRIPT_BASEDIR}/.."
+
+cargo build --release
---
+++ bin/dev.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+SCRIPT_BASEDIR=$(dirname "$0")
+
+which rustc &> /dev/null || { echo 'ERROR: rustc not found in PATH'; exit 1; }
+which cargo &> /dev/null || { echo 'ERROR: cargo not found in PATH'; exit 1; }
+
+cd "${SCRIPT_BASEDIR}/.."
+
+mkdir -p tmp
+
+export RUST_BACKTRACE=1
+export RUSTFLAGS=-Awarnings
+
+cargo run
