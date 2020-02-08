405b6cc6f9b865b21b9f00d708d95c5ffb4af4bc
Christian Mayer <christian@fox21.at>
2020-02-08 12:32:34 +0100

    Clean up.

--- bin/test.sh
+++ bin/test.sh
@@ -3,9 +3,7 @@
 SCRIPT_BASEDIR=$(dirname "$0")
 TEST_OPTS=${TEST_OPTS:-}
 
-which rustc &> /dev/null || { echo 'ERROR: rustc not found in PATH'; exit 1; }
 which cargo &> /dev/null || { echo 'ERROR: cargo not found in PATH'; exit 1; }
-which strip &> /dev/null || { echo 'ERROR: strip not found in PATH'; exit 1; }
 
 cd "${SCRIPT_BASEDIR}/.."
 
